4a4f90a73ae184f3582b234bdcb0636fabfd1922
marisol <mtoscanor37@gmail.com>
2019-03-23 09:25:20 +0000

    Created CRUD for class Category

--- models/category.rb
+++ models/category.rb
@@ -9,4 +9,48 @@ class Category
     @type = options['type']
   end
 
+  def save()
+    sql = "INSERT INTO categories (type)
+          VALUES ($1)
+          RETURNING id"
+    values = [@type]
+    category = SqlRunner.run(sql, values).first
+    @id = category['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE FROM categories
+          SET name = $1
+          WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM categories
+          WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM categories"
+    categories = SqlRunner.run(sql)
+    result = categories.map { |category| Category.new(category) }
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM categories"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM categories WHERE id = $1"
+    values = [id]
+    categories = SqlRunner.run(sql, values).first
+    result = Category.new(categories)
+    return result
+  end
+
 end
