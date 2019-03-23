27e7a11be10adcde21a7599832a35141720dc1ad
marisol <mtoscanor37@gmail.com>
2019-03-23 13:34:35 +0000

    Passing test for manufacturer_id in Product class

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ require_relative('../models/product.rb')
 require_relative('../models/manufacturer.rb')
 require_relative('../models/category.rb')
 
-
+Product.delete_all
+Manufacturer.delete_all
+Category.delete_all
 
 
 manufacturer1 = Manufacturer.new({
--- models/specs/product_spec.rb
+++ models/specs/product_spec.rb
@@ -7,6 +7,14 @@ require_relative('../category.rb')
 class TestProduct < MiniTest::Test
 
   def setup()
+    @manufacturer1 = Manufacturer.new({
+      "id" => 1,
+      "name" => "John Morrison Kilt",
+      "country" => "UK",
+      "phone" => "0131 556 473",
+      "email" => "info@johnmorrisonkilt.com"
+      })
+
     options = {
       "name" => "kilt",
       "description" => "8 yards polyester kilt",
@@ -47,11 +55,17 @@ class TestProduct < MiniTest::Test
   end
 
   def test_product_manufacturer_id()
-    assert_equal(1, @product1.manufacturer_id)
+    result = @manufacturer1.id
+    assert_equal(result, @product1.manufacturer_id)
   end
 
   def test_product_category_id()
     assert_equal(1, @product1.category_id())
+  end
+
+  def test_manufacturer_name_of_a_product()
+    result = @manufacturer1.key(name)
+    assert_equal(result, @product1.manufacturer())
   end
 
 end
