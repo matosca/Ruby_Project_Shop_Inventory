f6ff24ed407b6b667761f669df7ad2585b35841b
marisol <mtoscanor37@gmail.com>
2019-03-23 12:33:10 +0000

    Setting up controllers

--- inventory_controller.rb
+++ inventory_controller.rb
@@ -4,3 +4,9 @@ require_relative('/models/manufacturer.rb')
 require_relative('/models/category.rb')
 require_relative('/models/product.rb')
 also_reload('./models/*')
+
+
+get '/stock-list' do #INDEX
+  @products = Product.all()
+  erb( :index )
+end
