a6aeb3f5c996198fd661411a163139ffeb2961d3
marisol <mtoscanor37@gmail.com>
2019-03-23 20:07:02 +0000

    Added index and show routes to manufacturer_controller.rb

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -3,12 +3,23 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/manufacturer.rb' )
 also_reload( '../models/*' )
 
-get '/manufacturers' do
+get '/manufacturers' do #INDEX
   @manufacturers = Manufacturer.all()
   erb( :"manufacturers/index" )
 end
 
-get '/manufacturers/:id' do
+get '/manufacturers/new' do #NEW
+  erb( :"manufacturers/new" )
+end
+
+get '/manufacturers/:id' do #SHOW
   @manufacturer = Manufacturer.find(params[:id])
   erb( :"manufacturers/show" )
+end
+
+post'/manufacturers' do #CREATE
+  @manufacturer = Manufacturer.new(params)
+  @manufacturer.save()
+  # redirect to '/manufacturers'
+  erb( :"manufacturers/create" )
 end
