eaae65b340e7bfbf1a9f74cdcc86710dc428f4a6
marisol <mtoscanor37@gmail.com>
2019-03-23 09:24:18 +0000

    Created CRUD for class Manufacturer

--- models/manufacturer.rb
+++ models/manufacturer.rb
@@ -13,4 +13,52 @@ class Manufacturer
     @email = options['email']
   end
 
+  def save()
+    sql = "INSERT INTO manufacturers (name, country, phone, email)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id"
+    values = [@name, @country, @phone, @email]
+    manufacturer = SqlRunner.run(sql, values).first
+    @id = manufacturer['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE manufacturers
+          SET (name, country, phone, email) = ($1, $2, $3, $4)
+          WHERE id = $5"
+    values = [@name, @country, @phone, @email]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM manufacturers
+          WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM manufacturers"
+    manufacturer_data = SqlRunner.run(sql)
+    result = Manufacturer.map_items(manufacturer_data)
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM manufacturers"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM manufacturers WHERE id = $1"
+    values = [id]
+    manufacturer = SqlRunner.run(sql, values).first
+    result = Manufacturer.new(manufacturer)
+    return result
+  end
+
+#Helper method for mapping
+  def self.map_items(manufacturer_data)
+    return manufacturer_data.map { |manufacturer| Manufacturer.new(manufacturer) }
+  end
 end
