4e578a20840be1019cab648fd90421cb14d93f6f
marisol <mtoscanor37@gmail.com>
2019-03-23 21:05:42 +0000

    Solved problem with delete function

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -12,14 +12,32 @@ get '/manufacturers/new' do #NEW
   erb( :"manufacturers/new" )
 end
 
-get '/manufacturers/:id' do #SHOW
-  @manufacturer = Manufacturer.find(params[:id])
-  erb( :"manufacturers/show" )
-end
-
 post'/manufacturers' do #CREATE
   @manufacturer = Manufacturer.new(params)
   @manufacturer.save()
   # redirect to '/manufacturers'
   erb( :"manufacturers/create" )
+end
+
+get '/manufacturers/:id' do #SHOW
+  @manufacturer = Manufacturer.find(params[:id])
+  erb( :"manufacturers/show" )
+end
+
+get '/manufacturers/:id/edit' do
+  @manufacturer = Manufacturer.find(params[:id])
+  erb( :"manufacturers/edit" )
+end
+
+post  '/manufacturers/:id' do
+  @manufacturer = Manufacturer.new(params)
+  @manufacturer.update()
+  # redirect to '/manufacturers'
+  erb( :"manufacturer/update" )
+end
+
+post '/manufactureres/:id/delete' do
+  manufacturer = Manufacturer.find(params[:id])
+  manufacturer.delete()
+  redirect to '/manufacturers'
 end
