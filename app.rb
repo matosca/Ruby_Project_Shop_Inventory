90237f17f370281b124c211790db35224c9e5144
marisol <mtoscanor37@gmail.com>
2019-05-25 20:34:00 +0100

    Deploying project to Heroky

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/inventory_controller.rb')
 require_relative('controllers/manufacturer_controller')
 require_relative('controllers/category_controller')
